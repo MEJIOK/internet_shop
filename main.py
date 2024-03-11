35b482a850fe12438abafe6b92a4e1d8210c68be
Evgeniy Erokhov <evgeniy.erokhov@gmail.com>
2024-03-11 04:37:43 +0300

    Task 2 Added init and attributes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 class Category:
+    total_categories = 0  # Атрибут класса для хранения общего количества категорий
+    total_unique_products = 0  # Атрибут класса для хранения общего количества уникальных продуктов
+
     def __init__(self, name, description):
         self.name = name
         self.description = description
         self.products = []  # Пустой список для хранения товаров в категории
+        Category.total_categories += 1  # Увеличиваем общее количество категорий
 
 
 class Product:
@@ -11,3 +15,4 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+        Category.total_unique_products += 1  # Увеличиваем общее количество уникальных продуктов
